dbd95956d1ccc80a50c12e470e1c51cbf774c3a4
handras <bandie9100@gmail.com>
2015-12-08 12:33:33 +0100

    replace default link to last link

--- mysql_compat.php
+++ mysql_compat.php
@@ -6,19 +6,19 @@
  
 function mysql_compat_affected_rows($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_affected_rows($link);
 }
 
 function mysql_compat_client_encoding($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_character_set_name($link);
 }
 
 function mysql_compat_close($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_close($link);
 }
 
@@ -46,10 +46,7 @@ function mysql_compat_connect($server = NULL, $username = NULL, $password = NULL
 	}
 	
 	$link = mysqli_connect($host, $username, $password, $dbname, $port, $socket);
-	if(!$new_link)
-	{
-		$GLOBALS['mysql_compat_default_link'] = $link;
-	}
+	$GLOBALS['mysql_compat_last_link'] = $link;
 	return $link;
 }
 
@@ -116,13 +113,13 @@ function mysql_compat_drop_db($dbname, $link)
 
 function mysql_compat_errno($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_errno($link);
 }
 
 function mysql_compat_error($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_error($link);
 }
 
@@ -214,61 +211,61 @@ function mysql_compat_free_result($result)
 
 function mysql_compat_get_client_info($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_get_client_info($link);
 }
 
 function mysql_compat_get_host_info($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_get_host_info($link);
 }
 
 function mysql_compat_get_proto_info($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_get_proto_info($link);
 }
 
 function mysql_compat_get_server_info($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_get_server_info($link);
 }
 
 function mysql_compat_info($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_info($link);
 }
 
 function mysql_compat_insert_id($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_insert_id($link);
 }
 
 function mysql_compat_list_dbs($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysql_compat_query("SHOW DATABASES", $link);
 }
 
 function mysql_compat_list_fields($dbname, $tname, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysql_compat_query("SHOW COLUMNS FROM `".mysql_compat_escape_name($dbname)."`.`".mysql_compat_escape_name($tname)."`", $link);
 }
 
 function mysql_compat_list_processes($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysql_compat_query("SHOW PROCESSLIST", $link);
 }
 
 function mysql_compat_list_tables($dbname, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysql_compat_query("SHOW TABLES FROM `".mysql_compat_escape_name($dbname)."`", $link);
 }
 
@@ -317,19 +314,19 @@ function mysql_compat_pconnect($server = NULL, $username = NULL, $password = NUL
 
 function mysql_compat_ping($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_ping($link);
 }
 
 function mysql_compat_query($query, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_query($link, $query);
 }
 
 function mysql_compat_real_escape_string($string, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_real_escape_string($link, $string);
 }
 
@@ -369,19 +366,19 @@ function mysql_compat_result($result, $row_num, $field_def = NULL)
 
 function mysql_compat_select_db($dbname, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_select_db($link, $dbname);
 }
 
 function mysql_compat_set_charset($name, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_ser_charset($link, $name);
 }
 
 function mysql_compat_stat($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_stat($link);
 }
 
@@ -408,12 +405,12 @@ function mysql_compat_tablename($tblist_result, $row_num)
 
 function mysql_compat_thread_id($link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_thread_id($link);
 }
 
 function mysql_compat_unbuffered_query($query, $link = NULL)
 {
-	if(!isset($link)) $link = $GLOBALS['mysql_compat_default_link'];
+	if(!isset($link)) $link = $GLOBALS['mysql_compat_last_link'];
 	return mysqli_query($link, $query, MYSQLI_USE_RESULT);
 }
